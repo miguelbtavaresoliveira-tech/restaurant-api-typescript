48fe9062ca2c17b2567378103736f10fd85c8d2d
miguelbtavaresoliveira-tech <miguel.btavaresoliveira@gmail.com>
2026-09-08 20:56:36 -0300

    feat: dependencies of rate-limit

--- src/domain/auth/auth.routes.ts
+++ src/domain/auth/auth.routes.ts
@@ -1,11 +1,22 @@
 import { FastifyInstance } from "fastify";
 import { authController } from './controllers/auth.controller.js'
 import { authenticate } from '../../shared/middlewares/authenticate.js'
+import fastifyRateLimit from '@fastify/rate-limit';
+
 
 export async function authRoutes(fastify: FastifyInstance) {
 
+    await fastify.register(fastifyRateLimit, { global: false });
+
     // Rota de login deve ser pública
-    fastify.post("/login", authController.login)
+    fastify.post("/login",{
+        config: {
+            rateLimit:{
+                max: 10,
+                timeWindow: '15 minutes'
+            }
+        }
+    }, authController.login)
 
     fastify.post("/forgot-password", authController.forgotPassword)
 

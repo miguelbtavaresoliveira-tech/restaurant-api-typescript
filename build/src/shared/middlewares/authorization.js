b81f69952e257a44e35396e9c7aa2c9b58fde8e9
miguelbtavaresoliveira-tech <miguel.btavaresoliveira@gmail.com>
2026-09-12 20:18:01 -0300

    chore: remove tracked files based on .gitignore

--- build/src/shared/middlewares/authorization.js
+++
@@ -1,8 +0,0 @@
-export function authorize(roles) {
-    return async (request, reply) => {
-        const userRole = request.user?.role;
-        if (!userRole || !roles.includes(userRole)) {
-            return reply.status(403).send({ message: "Acesso negado" });
-        }
-    };
-}
--- build/src/shared/utils/errors/formatZodErrors.js
+++
@@ -1,4 +0,0 @@
-import { z } from 'zod';
-export function formatError(result) {
-    return z.treeifyError(result.error);
-}
